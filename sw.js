20eb58bfb45213ce5c43566bdd88e304956a9dde
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2026-07-23 00:31:18 +0200

    OpoWeb v0.27.8: ampliar caché UC3M hasta tema 15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'opoweb-v2-0.27.7';
+const CACHE = 'opoweb-v2-0.27.8';
 const CORE_ASSETS = [
   './',
   './index.html',
@@ -63,7 +63,7 @@ async function optionalAssets() {
   const groups = await Promise.all([
     programmeAssets('./data/programa.json', 'content/la-puebla', 19),
     programmeAssets('./data/programa-diputacion-administrativo-2026.json', 'content/diputacion-toledo', 40),
-    programmeAssets('./data/programa-uc3m-auxiliar-administrativa-2026.json', 'content/uc3m', 14)
+    programmeAssets('./data/programa-uc3m-auxiliar-administrativa-2026.json', 'content/uc3m', 15)
   ]);
   return [...new Set(groups.flat())];
 }
